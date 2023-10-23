dd9d31f46372820c3f3e51503a9109d444c5d7a1
merrillj-byui <mer23010@byui.edu>
2023-10-22 22:45:19 -0600

    Got rid of the separate hide function

--- project/CountryCard.js
+++ project/CountryCard.js
@@ -26,95 +26,53 @@ const CountryCard = {
         // Display this randomly selected country's information on the card
         this.displayCountry();
     },
-    displayCountry : function () {
+    displayCountry : function (hide=true) {
         // Display the information on the card for the country associated with the global index
         var country = this.countries[this.index];
-        // Show the flag, country name, and country abbreviation
-        document.querySelector("#name").textContent = `${country.flag}
-                                                       ${country.name.common}
-                                                       (${country.cca2})`;
-        // Show the region
-        document.querySelector("#region").textContent = country.region;
-        // Show the subregion
-        document.querySelector("#subregion").textContent = country.subregion;
-        // Show the capital
-        document.querySelector("#capital").textContent = country.capital;
-        // Build the list of languages
-        let markup = "";
-        for (var language in country.languages){
-            markup += `• ${country.languages[language]}<br>`;
-            console.log("language:", language);
-        } 
-        // Show the languages      
-        document.querySelector("#languages").innerHTML = markup;
-    },
-    hideFields : function () {
-        // Based on the selected options (multiple select), hide the associated information
-        var country = this.countries[this.index];
         const selectElement = document.querySelector("#hide");
-        var blanks = "_";
-        // Get an array of the selected options
+        const blank = "_";
+        // Get the array of selected fields to hide
         const selected = Array.from(selectElement.selectedOptions).map(option => option.value);
         console.log("debug:", selected); // debugging
-        // Hide the name, if that is selected
-        if (selected.includes("name"))
-        {
-            document.querySelector("#name").textContent = `${country.flag} 
-                                                           ${blanks.repeat(country.name.common.length)}
-                                                           (${country.cca2})`;
-        }
-        // Hide the abbreviation, if that is selected
-        if (selected.includes("abbr"))
-        {
-            document.querySelector("#name").textContent = `${country.flag} 
-                                                           ${country.name.common} 
-                                                           (${blanks.repeat(country.cca2.length)})`;
-        }
-        // Hide the region, if that is selected
-        if (selected.includes("region"))
-        {
-            document.querySelector("#region").textContent = blanks.repeat(country.region.length);
-        }
-        // Hide the subregion, if that is selected
-        if (selected.includes("subregion"))
-        {
-            document.querySelector("#subregion").textContent = blanks.repeat(country.subregion.length);
-        }
-        // Hide the capital, if that is selected
-        if (selected.includes("capital"))
-        {
-            document.querySelector("#capital").textContent = blanks.repeat(country.capital[0].length);
-        }
-        // Hide the languages, if that is selected
-        if (selected.includes("languages"))
-        {
-            //document.querySelector("#languages").innerHTML = "";
-            let markup = "";
-            for (var language in country.languages){
-                markup += `• ${blanks.repeat(country.languages[language].length)}<br>`;
-                console.log("language:", language); // debugging
-            } 
-            document.querySelector("#languages").innerHTML = markup;
-        }
-   }
+        // Get the values to display (or blanks if they should be hidden)
+        const name = hide == true && selected.includes("name") ? blank.repeat(country.name.common.length) : country.name.common;
+        const abbr = hide == true && selected.includes("abbr") ? blank.repeat(country.cca2.length) : country.cca2;
+        const region = hide == true && selected.includes("region") ? blank.repeat(country.region.length) : country.region;
+        const subregion = hide == true && selected.includes("subregion") ? blank.repeat(country.subregion.length) : country.subregion;
+        const capital = hide == true && selected.includes("capital") ? blank.repeat(country.capital[0].length) : country.capital;
+        // Loop through languages
+        let markup = "";
+        for (var key in country.languages){
+            let language = country.languages[key];
+            markup += hide == true && selected.includes("languages") ? `• ${blank.repeat(language.length)}` : `• ${language}`;
+            markup += `<br>`;
+            console.log("language:", language);
+        } 
+        // Show the country information      
+        document.querySelector("#name").textContent = `${country.flag} ${name} (${abbr})`;
+        document.querySelector("#region").textContent = region;
+        document.querySelector("#subregion").textContent = subregion;
+        document.querySelector("#capital").textContent = capital;
+        document.querySelector("#languages").innerHTML = markup;
+    },
 }
 
 document.querySelector("#hide").addEventListener("change", () => {
     // Callback for the selector of information to hide
     CountryCard.displayCountry();
-    CountryCard.hideFields();
 });
 
 document.querySelector("#next").addEventListener("click", () => {
     // Callback for the button to show the next random country
     CountryCard.displayRandomCountry();
-    CountryCard.hideFields();
+    CountryCard.displayCountry();
 });
 
 document.querySelector("#unhide").addEventListener("click", () => {
     // Callback for the button to unhide all hidden fields on the currently displayed country
     // This purposely does NOT clear the selected fields to hide, so you can go to the next country
     // and the fields that were selected will already be hidden.
-    CountryCard.displayCountry()});
+    CountryCard.displayCountry(false);
+});
 
 export default CountryCard
